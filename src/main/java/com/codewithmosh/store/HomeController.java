ff4b43d7b949a0d3a1e19387ebc65c8984ebffe8
Siwakorn Srisakaokul <sping128@gmail.com>
2026-06-19 22:22:47 +0700

    How to access application.properties

--- src/main/java/com/codewithmosh/store/HomeController.java
+++ src/main/java/com/codewithmosh/store/HomeController.java
@@ -1,18 +1,18 @@
 package com.codewithmosh.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
-        String viewName = getViewName();
-        return viewName;
-    }
-
-    private String getViewName() {
+        System.out.println("appName: " + appName);
         return "index.html";
     }
+
 }
