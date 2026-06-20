f461d39679e46b3a5b8eb8641770d74dece3f7b3
Siwakorn Srisakaokul <sping128@gmail.com>
2026-06-20 11:58:53 +0700

    Learn Dto

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.services.UserService;
 
 import lombok.AllArgsConstructor;
-
 
 @AllArgsConstructor
 @RestController
@@ -21,15 +20,16 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers() {
+    public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user = userService.getUser(id);
         if (user == null)
             return ResponseEntity.notFound().build();
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/codewithmosh/store/services/UserService.java
+++ src/main/java/com/codewithmosh/store/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 
@@ -14,8 +15,9 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
     private final UserRepository userRepository;
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())).toList();
     }
 
     public User getUser(Long id) {
